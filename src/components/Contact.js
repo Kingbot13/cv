754f8aaff868aeae89372b6451fdabad36915773
Dylan King <dwkblue51@yahoo.com>
2022-01-07 13:59:17 -0600

    hardcode p tags in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,20 @@
 import React, {Component} from "react";
 
 class Contact extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            items: [],
-            text: ''
-        }
-    }
 
 
     render(){
-        const {contacts} = this.props;
+        const {contact} = this.props;
         return(
             <div>
-                {contacts.map((info) =>{
-                    return <p key={info.id}>{info.text}</p>
-                })}
+                <p>{contact.firstName}</p>
+                <p>{contact.lastName}</p>
+                <p>{contact.email}</p>
+                <p>{contact.phone}</p>
+                <p>{contact.address}</p>
+                <p>{contact.city}</p>
+                <p>{contact.st}</p>
+                <p>{contact.zip}</p>
             </div>
         )
     }
